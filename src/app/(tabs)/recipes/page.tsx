d2ce0a8f8fdbaf4f5a01ff678f824bb475bac38d
shooo0ki <shoki041124ss@gmail.com>
2025-12-17 23:41:50 +0900

    use client

--- src/app/(tabs)/recipes/page.tsx
+++ src/app/(tabs)/recipes/page.tsx
@@ -1,3 +1,5 @@
+ "use client";
+
 import { useMemo, useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
